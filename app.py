c2eaa695cd6412b966406f04e64b636076fcbac8
rohan junna <rohanjunna846@gmail.com>
2026-09-04 19:32:01 +0530

    Prepare Flask app for deployment

--- app.py
+++ app.py
@@ -698,5 +698,7 @@ def update_user_plan(user_id):
 
 
 if __name__ == "__main__":
-    initialize_database()
-    app.run(debug=True, port=int(os.environ.get("PORT", "5000")))
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
